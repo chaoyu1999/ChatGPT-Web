f1f7424898c564e54b392bf85c71b6299f59fab8
cy <chaoyu1999@qq.com>
2023-06-29 21:59:41 +0800

    添加权限

--- app/store/access.ts
+++ app/store/access.ts
@@ -26,7 +26,7 @@ export const useAccessStore = create<AccessControlStore>()(
       token: "",
       accessCode: "",
       needCode: true,
-      hideUserApiKey: false,
+      hideUserApiKey: true,
       openaiUrl: "/api/openai/",
 
       enabledAccessControl() {
@@ -45,7 +45,7 @@ export const useAccessStore = create<AccessControlStore>()(
 
         // has token or has code or disabled access control
         return (
-          !!get().token || !!get().accessCode || !get().enabledAccessControl()
+          !!get().accessCode || !get().enabledAccessControl()
         );
       },
       fetch() {
