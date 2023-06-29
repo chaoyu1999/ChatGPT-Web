26400b21abe42c365a69b8539cc0b336da94d00f
cy <chaoyu1999@qq.com>
2023-06-29 22:14:02 +0800

    add auth

--- app/api/auth.ts
+++ app/api/auth.ts
@@ -37,12 +37,11 @@ export function auth(req: NextRequest) {
   console.log("[Auth] allowed hashed codes: ", [...serverConfig.codes]);
   console.log("[Auth] got access code:", accessCode);
   console.log("[Auth] server access code:", serverConfig.code);
-
   console.log("[Auth] hashed access code:", hashedCode);
   console.log("[User IP] ", getIP(req));
   console.log("[Time] ", new Date().toLocaleString());
 
-  if (serverConfig.needCode && serverConfig.code != accessCode){//!serverConfig.codes.has(hashedCode) ) {
+  if (serverConfig.needCode && !serverConfig.codes.has(hashedCode)){
     return {
       error: true,
       needAccessCode: true,
