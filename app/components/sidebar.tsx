1a97f1542b9259c454884f4ff5f08e6a9bde2594
cy <chaoyu1999@qq.com>
2024-01-19 16:11:47 +0800

    23333

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -160,7 +160,7 @@ export function SideBar(props: { className?: string }) {
         <div style={horizontalLineStyle}></div>
 
         <div className={styles["sidebar-sub-title"]}>
-          <span style={{ fontSize: "15px", ...rainbowTextStyle }}>接入bing 模型（GPT-4）：【bing-不联网 || bing-联网版】。联网版：结合网页搜索结果回答，可搜索最新问题。不联网：只使用GPT-4内部知识回答，更精确，但只能回答2021年之前的内容。</span>
+          <span style={{ fontSize: "17px", ...rainbowTextStyle }}>接入bing 模型（GPT-4）：【bing-不联网 || bing-联网版】。联网版：结合网页搜索结果回答，可搜索最新问题。不联网：只使用GPT-4内部知识回答，更精确，但只能回答2021年之前的内容。</span>
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
