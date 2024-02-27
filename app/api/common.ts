07d2485461c91c5e5627f6945f317caa463adefb
cy <chaoyu1999@qq.com>
2024-02-27 09:20:29 +0800

    gpt-3.5-turbo-1106

--- app/api/common.ts
+++ app/api/common.ts
@@ -124,7 +124,7 @@ export async function requestOpenai(req: NextRequest) {
       // 检查请求体中是否包含对 GPT-3.5 模型的请求
       if ((jsonBody?.model ?? "").includes("3.5")) {
         // 如果使用了 GPT-3.5 模型，更改模型名称为 GPT-3.5-turbo-1106
-        jsonBody.model = "gpt-3.5-turbo";
+        jsonBody.model = "gpt-3.5-turbo-1106";
         // 更新 fetchOptions.body 为修改后的 jsonBody
         fetchOptions.body = JSON.stringify(jsonBody);
         console.log("[Model]:", "Use gpt-3.5-turbo model!");
